c7ecbff0ddc2b63975bae4d799c320291cd5814e
ayiio <61615400+ayiio@users.noreply.github.com>
2023-06-29 23:58:47 +0800

    Update selectsort.go

--- doc_算法图解_go/section2/selectSort/selectsort.go
+++ doc_算法图解_go/section2/selectSort/selectsort.go
@@ -1,8 +1,8 @@
 package selectsort
 
 func findSmaller(arr []int) (smaller_index int) {
-	smalller := arr[0]
-	smaller_index = 0
+	smalller := arr[0]  //最小值
+	smaller_index = 0   //最小值索引
 	for i := 1; i < len(arr); i++ {
 		if arr[i] < smalller {
 			smalller = arr[i]
@@ -16,7 +16,7 @@ func SelectSort(arr []int) []int {
 	var newArr []int
 	leno := len(arr)
 	for i := 0; i < leno; i++ {
-		smallidx := findSmaller(arr)
+		smallidx := findSmaller(arr)  //找到数组中最小元素，将其加入新数组中
 		newArr = append(newArr, arr[smallidx])
 		arr = append(arr[:smallidx], arr[smallidx+1:]...)
 	}
