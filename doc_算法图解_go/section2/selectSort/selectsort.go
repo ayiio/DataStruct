502c17d737578e980dda1e114b227cf4cf368f54
ayiio <61615400+ayiio@users.noreply.github.com>
2023-06-29 23:54:09 +0800

    Create selectsort.go

---
+++ doc_算法图解_go/section2/selectSort/selectsort.go
@@ -0,0 +1,23 @@
+package selectsort
+
+func findSmaller(arr []int) (smaller_index int) {
+	smalller := arr[0]
+	smaller_index = 0
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < smalller {
+			smalller = arr[i]
+			smaller_index = i
+		}
+	}
+	return
+}
+
+func SelectSort(arr []int) []int {
+	var newArr []int
+	for i := 0; i < len(arr); i++ {
+		smallidx := findSmaller(arr)
+		newArr = append(newArr, arr[smallidx])
+		arr = append(arr[:smallidx], arr[smallidx+1:]...)
+	}
+	return newArr
+}
